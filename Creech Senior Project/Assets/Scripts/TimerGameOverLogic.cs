f1bb2181e81f4c6f3f29d6f23f8f858ae9ef874a
Robert Creecg <rmcreech12@gmail.com>
2024-02-15 21:52:17 -0500

    Updates

---
+++ Creech Senior Project/Assets/Scripts/TimerGameOverLogic.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerGameOverLogic : MonoBehaviour
+{
+    int countDownStartValue = 1500;
+    public Text timerUI;
+
+    //Start is called before the first update
+    void Start()
+    {
+        countDownTimer();
+    }
+    
+    void countDownTimer()
+    {
+        if(countDownStartValue > 0)
+        {
+            TimeSpan spanTime = TimeSpan.FromSeconds(countDownStartValue);
+            timerUI.text = "Timer: " + spanTime.Minutes + " : " + spanTime.Seconds;
+            countDownStartValue--;
+            Invoke("countDownTimer", 1.0f);
+        }
+        else
+        {
+            timerUI.text = "GameOver!";
+        }
+    }
+}
