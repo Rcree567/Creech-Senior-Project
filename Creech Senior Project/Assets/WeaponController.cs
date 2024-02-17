57a6695ad5e8b235377fd763244a9b3561d69b88
Robert Creecg <rmcreech12@gmail.com>
2024-02-16 20:49:36 -0500

    Updates

---
+++ Creech Senior Project/Assets/WeaponController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject Sword;
+    public bool CanAttack = true;
+    public float AttackCooldown = 1.0f;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(CanAttack)
+            {
+                SwordAttack();
+            }
+        }
+    }
+
+    public void SwordAttack()
+    {
+        CanAttack = false;
+        Animator anim = Sword.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+    }
+
+
+    IEnumerator ResetAttackCooldown()
+    {
+        yield return new WaitForSeconds(AttackCooldown);
+        CanAttack = true;
+    }
+
+
+}
