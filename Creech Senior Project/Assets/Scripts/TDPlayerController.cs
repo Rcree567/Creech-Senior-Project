7d15501db1cb792720a9f00cc7efd01d65659d67
Robert Creecg <rmcreech12@gmail.com>
2024-03-08 19:00:01 -0500

    Updates

--- Creech Senior Project/Assets/Scripts/TDPlayerController.cs
+++ Creech Senior Project/Assets/Scripts/TDPlayerController.cs
@@ -1,18 +1,51 @@
 using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class TDPlayerController : MonoBehaviour
 {
+    public float speed;
+
+    private Rigidbody rb;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+    private Camera mainCamera;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
+        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * speed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveVelocity;
     }
 }
---
+++ Creech Senior Project/Assets/Scripts/TDWeaponController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDWeaponController : MonoBehaviour
+{
+
+    public GameObject Sword;
+    public bool CanAttack = true;
+    public float AttackCooldown = 1.0f;
+    public AudioClip SwordAttackSound;
+    public float attackDistance = 3f;
+    public float attackDelay = 0.4f;
+    public float attackSpeed = 1f;
+    public int attackDamage = 1;
+    public LayerMask attackLayer;
+
+    bool attacking = false;
+    bool readyToAttack = true;
+    int attackCount;
+
+    public int damage = 10;
+
+
+    public void Attack()
+    {
+        if (!readyToAttack || attacking) return;
+
+        readyToAttack = false;
+        attacking = true;
+
+        Invoke(nameof(ResetAttack), attackSpeed);
+
+    }
+
+    void ResetAttack()
+    {
+        attacking = false;
+        readyToAttack = true;
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (CanAttack)
+            {
+                SwordAttack();
+            }
+        }
+    }
+
+    public void SwordAttack()
+    {
+        CanAttack = false;
+        Animator anim = Sword.GetComponent<Animator>();
+        anim.SetTrigger("Attack");
+        AudioSource ac = GetComponent<AudioSource>();
+        ac.PlayOneShot(SwordAttackSound);
+        StartCoroutine(ResetAttackCooldown());
+    }
+
+
+    IEnumerator ResetAttackCooldown()
+    {
+        yield return new WaitForSeconds(AttackCooldown);
+        CanAttack = true;
+    }
+
+  
+
+}
