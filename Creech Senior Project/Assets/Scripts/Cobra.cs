8bd464ad29efaad6b07e9d90aaa9da006ef7f943
Robert Creecg <rmcreech12@gmail.com>
2024-04-25 15:55:58 -0400

    Updates

---
+++ Creech Senior Project/Assets/Scripts/Cobra.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cobra : MonoBehaviour
+{
+    private Transform Player;
+
+    public int maxHealth = 100;
+    public int Health;
+
+    public float damage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        Health = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Sword")
+        {
+            maxHealth -= 10;
+
+            if (maxHealth <= 0)
+            {
+                Die();
+            }
+
+        }
+
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
